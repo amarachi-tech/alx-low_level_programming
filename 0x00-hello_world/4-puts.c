9627516e68c7168f34eef7bd7b588fe382b6260a
Josephine <josephineamarachi616@gmail.com>
2022-09-10 14:01:42 -0700

    C program that prints a text without "printf"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entery point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0)
+	return (0);
 }
