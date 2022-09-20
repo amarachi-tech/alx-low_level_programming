3ec7b56d2c3cca9dde3af5d6c54533ffaa0e90ac
Josephine <josephineamarachi616@gmail.com>
2022-09-20 03:23:48 -0700

    Write a function that swaps the values of two integers.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers
+ *
+ * @a: an integer
+ *
+ * @b: an integer
+ *
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
